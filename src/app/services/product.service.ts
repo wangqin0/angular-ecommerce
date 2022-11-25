1ae70dfa802d2b1e77c96b527833ae1135e654d3
Wang Qin <wangqin0@outlook.com>
2022-11-24 22:23:15 -0800

    Search for Products by Category, dynamic 2. Create class ProductCategory and a new component ProductCategoryMenuComponent 3. Update ProductService to call `http://localhost:8080/api/product-category` 4. Add component `product-category-menu` subscribe to productService.getProductCategories() to read category data 5. Replace original side menu with `app-product-category-menu`, build table with *ngFor

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Product} from "../common/product";
 import {map} from 'rxjs/operators'
+import {ProductCategory} from "../common/product-category";
 
 @Injectable({
   providedIn: 'root'
@@ -11,23 +12,36 @@ import {map} from 'rxjs/operators'
 export class ProductService {
 
   // for size other than default: ?size=100
-  private baseUrl = "http://localhost:8080/api/products";
+  private productUrl = "http://localhost:8080/api/products";
+  private categoryUrl = "http://localhost:8080/api/product-category";
 
   constructor(private httpClient: HttpClient) { }
 
   getProductList(categoryId: number): Observable<Product[]> {
 
     // DONE: need to build URL based on category id
-    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`
+    const searchUrl = `${this.productUrl}/search/findByCategoryId?id=${categoryId}`
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    )
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     )
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
