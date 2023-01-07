af6de123f3fc2b53a96dc1a640498fa2deeacfff
Wang Qin <wangqin0@outlook.com>
2023-01-06 17:31:37 -0800

    HTTPS Frontend 1. Generate key and self-signed certificate 2. Run Angular App with the key and self-signed certificate 3. Update Spring Boot app with new URL 4. Update Okta   - Redirect URI in Angular   - Redirect URIs in Okta Dashboard   - Update API Trusted Origins in Okta Dashboard

--- src/app/config/auth-config.ts
+++ src/app/config/auth-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa7suq80tJySq1i55d7',
         issuer: 'https://dev-95947893.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email'],
     }
 }
