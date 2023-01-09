f15185713151f39263299c96ca03eb0ba90ae63f
Wang Qin <wangqin0@outlook.com>
2023-01-09 00:21:16 -0800

    sync 129.159.46.95

--- src/app/config/auth-config.ts
+++ src/app/config/auth-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa7suq80tJySq1i55d7',
         issuer: 'https://dev-95947893.okta.com/oauth2/default',
-        redirectUri: 'https://localhost:4200/login/callback',
+        redirectUri: 'https://129.159.46.95:4200/login/callback',
         scopes: ['openid', 'profile', 'email'],
     }
 }
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  backendUrl: "https://localhost:8443/api",
+  backendUrl: "https://129.159.46.95:8080/api",
   stripePublishableKey: "pk_test_51MFCnIIaI4CkWjpBZTCeNK66ma2cWm9ILWJH8wgIXavu7JfVeGo3VSk32UNBEJreok22Rb4ofPWWEkmzCHuDZSkK00EQcDmGPT",
 };
