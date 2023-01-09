cf37fcd7f7bec9b6561509d53c7566885c7bf773
Wang Qin <wangqin0@outlook.com>
2023-01-09 00:31:42 -0800

    sync 129.159.46.95

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  backendUrl: "https://129.159.46.95:8080/api",
+  backendUrl: "http://129.159.46.95:8080/api",
   stripePublishableKey: "pk_test_51MFCnIIaI4CkWjpBZTCeNK66ma2cWm9ILWJH8wgIXavu7JfVeGo3VSk32UNBEJreok22Rb4ofPWWEkmzCHuDZSkK00EQcDmGPT",
 };
